2cbf3f7dd7100c65e89192ba20f78c614a763d5d
JT <jt@jtonto.com>
2020-10-24 14:22:46 -0500

    included lesson 2. Has bad handling of age

--- 1Program.py
+++ 1Program.py
@@ -1,3 +1,5 @@
+#Doing stuff with input output
+
 numberOfTimes= 5
 anyName= 'Chuck'
 '''
@@ -23,7 +25,7 @@ else:
     print('This is not true')
 '''
 flag= True
-
+'''
 while flag== True:
     anyName= input('What is your name? (Type "stop" to stop) ')
     if anyName.lower()!= "stop":
@@ -31,5 +33,80 @@ while flag== True:
     else:
         flag= False
         print('Have a nice day.')
+'''   
     
-    
+#Doing stuff with input and output; but make it fancy
+'''Takes a users name, age, email address'''
+'''Name[string]     Age[int]   Email Address[string]'''
+''' no numbers        >9        text@text.domain'''
+def gatherDetails():
+    while True:
+        #Get Name
+        try:
+            myName= str(input('What is your name? '))
+        except:
+            print(f'{myName} is not the kind of name I recognize. Please try again')
+        #Get Age
+        try:
+            myAge= int(input('What is your age? '))
+        except:
+            myAge= int(input('Please try a number '))
+        #Get Email
+        try:
+            myEmail= str(input('Please enter your email '))
+        except:
+            print(f'{myEmail} is not the kind of name I recognize. Please try again')
+        
+        return myName, myAge, myEmail
+
+
+
+
+testName, testAge, testEmail= gatherDetails()
+print(f'My test name is {testName} My test Age is {testAge} and my test email is {testEmail}')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+def gatherDetails():
+    myName= input('What is your name? ')
+    myAge= input('What is your age? ')
+    myEmail= input('What is your email? ')
+
+
+
+
+
+
+
+
+try:
+    myName= str(myName)
+except:
+    print(f'{myName} is not the kind of name I recognize. Please try again')
+
+'''
